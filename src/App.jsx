53edee8a845296e376a7405e91386c8a35cbb086
oihane.epelde <oihane.epelde@alumni.mondragon.edu>
2022-05-24 10:51:29 +0200

    Sonarqube fixes+json

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import Registro from "./components/Registro.jsx";
 import DashBoard from "./components/Dashboard.jsx"; 
 import Eventos from "./components/Eventos.jsx"; 
 
-/*import Sidebar from "./components/componentsDashBoard/Sidebar.jsx"*/
-
 export function App() {
 
   return (
--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -1,35 +1,22 @@
 import React from "react";
 
 /*ICONOS*/
-import { ImNewspaper } from "react-icons/im";
-import { MdEvent } from "react-icons/md"
-import { MdPlace } from "react-icons/md"
-import { CgProfile } from "react-icons/cg"
-import { ImCross} from "react-icons/im"
-import { FiLogOut } from "react-icons/fi"
-import { FiMenu } from "react-icons/fi";
-import { GrCircleInformation } from "react-icons/gr"
+import { FiMenu } from "react-icons/fi"
 import { FaPlus } from "react-icons/fa"
 
-
-import { Helmet } from "react-helmet"
 /*IMAGENES*/
 import logo from "../images/logo.png"
 import img_playa from "../images/PlayaConcha.jpg"
 
+import Aside from "./componentsDashBoard/Aside.jsx"
+
 
 import "../styles/styleSidebar.css"
 import "../styles/styleEventos.css"
-
+import data from "../data/eventos.json";
 
 
 function Eventos() {
-  //const sideMenu = document.querySelector("aside");
-  //const closeBtn = document.querySelector("close-btn");
-  
-  //const closeBtn = document.getElementById("close-btn");
-
-  //const {toggle, setToggle} = useState(true);
 
   function abrirNavbar(){
 
@@ -37,106 +24,75 @@ function Eventos() {
     document.getElementById("asidee").classList.add("abrir");
   }
 
-  function cerrarNavbar(){
+ 
 
-    document.getElementById("asidee").classList.remove("abrir");
-    document.getElementById("asidee").classList.add("cerrar");
-  }
-
-  /*closeBtn.addEventListener("click", () => {
-    console.log("Cerrar botoia");
-    sideMenu.classList.remove("irekia");
-    sideMenu.classList.add("itxia");
-  })*/
+  const DisplayData=data.map(
+    (evento)=>{
+      return(
+        <div className="eventCard destacado" key={evento.id}>
+          <p>{evento.nombre}</p>
+          <p>{evento.fecha}</p>
+          <p>11:30</p>
+          <p>{evento.lugar}</p>
+          <div className="imagenCard">
+            <img src={img_playa} alt="playa1"></img>
+          </div>
+          <button className="buttonDetalles">Detalles</button>
+        </div>
+              
+      );
+    } 
+  )
+  
 
   return(
     <div className="containerEventos">
-      <aside id="asidee" className="cerrar">
-        <div className = "top">
-          <div className="close" id="close-btn">
-            <button onClick={cerrarNavbar}><ImCross /></button>
-          </div>
-          <img className="imagenLogo" src={logo} alt="Logo"/>
-          <h2>NatureOps</h2>  
-        </div>
-        <div className="sidebar">
-          <a href="https://www.google.es/">
-            <span> <ImNewspaper /></span>
-            <h3>News</h3>
-          </a>
-          <a href="/eventos">
-            <span> <MdEvent /></span>
-            <h3>Events</h3>
-          </a>
-          <a href="https://www.google.es/">
-            <span> <MdPlace /></span>
-            <h3>Places</h3>
-          </a>
-          <a href="https://www.google.es/">
-            <span> <CgProfile /></span>
-            <h3>Profile</h3>
-          </a>
-          <a className="salir" href="/login">
-            <span> <FiLogOut /></span>
-            <h3>Logout</h3>
-          </a>
-        </div>
-      </aside>
+      <Aside/>
       <main>
         <h1>Eventos</h1>
         <div className="eventos">
           <div className="apartado">
             <h2>Lista de eventos disponibles</h2>
-            <GrCircleInformation/>
           </div>
           <div className="listaEventos">
-            <div className="eventCard destacado">
-              <p>Evento GreenPeace</p>
-              <p>2022-05-20</p>
-              <p>11:30</p>
-              <p>Playa de la Concha, Donostia</p>
-              <div className="imagenCard">
-                <img src={img_playa} alt="playa"></img>
-              </div>
-              <button className="buttonDetalles">Detalles</button>
-            </div>
+            {DisplayData}
             <div className="eventCard destacado">
               <p>Evento Carton</p>
-              <p>2022-05-20</p>
-              <p>11:30</p>
-              <p>Playa de la Concha, Donostia</p>
+              <p>2022-02-12</p>
+              <p>12:30</p>
+              <p>Playa de Zurriola, Donostia</p>
               <div className="imagenCard">
-                <img src={img_playa} alt="playa"></img>
+                <img src={img_playa} alt="playa2"></img>
               </div>
               <button className="buttonDetalles">Detalles</button>
             </div>
             <div className="eventCard">
               <p>Evento Papel</p>
-              <p>2022-05-20</p>
-              <p>11:30</p>
-              <p>Playa de la Concha, Donostia</p>
+              <p>2022-07-23</p>
+              <p>15:30</p>
+              <p>Playa de Santiago, Deba</p>
               <div className="imagenCard">
-                <img src={img_playa} alt="playa"></img>
+                <img src={img_playa} alt="playa3"></img>
               </div>
               <button className="buttonDetalles">Detalles</button>
             </div>
             <div className="eventCard">
-              <p>Evento Papel</p>
-              <p>2022-05-20</p>
-              <p>11:30</p>
-              <p>Playa de la Concha, Donostia</p>
+              <p>Evento Plastico</p>
+              <p>2022-04-20</p>
+              <p>17:30</p>
+              <p>Playa de Getaria, Getaria</p>
               <div className="imagenCard">
-                <img src={img_playa} alt="playa"></img>
+                <img src={img_playa} alt="playa4"></img>
               </div>
               <button className="buttonDetalles">Detalles</button>
             </div>
             <div className="eventCard">
-              <p>Evento Papel</p>
-              <p>2022-05-20</p>
-              <p>11:30</p>
-              <p>Playa de la Concha, Donostia</p>
+              <p>Evento Beluga</p>
+              <p>2022-08-24</p>
+              <p>18:30</p>
+              <p>Playa Grande, Zarautz</p>
               <div className="imagenCard">
-                <img src={img_playa} alt="playa"></img>
+                <img src={img_playa} alt="playa5"></img>
               </div>
               <button className="buttonDetalles">Detalles</button>
             </div>          
@@ -157,12 +113,7 @@ function Eventos() {
             </div>
           </div>
         </div>
-      </div>
-      <Helmet>
-       
-      </Helmet>
-      
-      
+      </div> 
     </div>
   );
 }
