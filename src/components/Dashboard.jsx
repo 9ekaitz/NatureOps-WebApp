0df9aeba8adb80e046cc6689fa2619f5979d0cd1
oihane.epelde <oihane.epelde@alumni.mondragon.edu>
2022-05-24 12:48:59 +0200

    Terminado+known fixes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 /*, { useState }*/
-/*import Aside from "./componentsDashBoard/Aside.jsx";*/
+import Aside from "./componentsDashBoard/Aside.jsx";
 /*ICONOS*/
-import { ImNewspaper } from "react-icons/im";
-import { MdEvent } from "react-icons/md";
-import { MdPlace } from "react-icons/md";
-import { CgProfile } from "react-icons/cg";
-import { ImCross} from "react-icons/im";
-import { FiLogOut } from "react-icons/fi";
 import { FiMenu } from "react-icons/fi";
 import { GrCircleInformation } from "react-icons/gr";
 
@@ -30,45 +24,9 @@ function DashBoard() {
     document.getElementById("asidee").classList.add("abrir");
   }
 
-  function cerrarNavbar(){
-
-    document.getElementById("asidee").classList.remove("abrir");
-    document.getElementById("asidee").classList.add("cerrar");
-  }
-
   return(
     <div className="container">
-      <aside id="asidee" className="cerrar">
-        <div className = "top">
-          <div className="close" id="close-btn">
-            <button onClick={cerrarNavbar}><ImCross /></button>
-          </div>
-          <img className="imagenLogo" src={logo} alt="Logo"/>
-          <h2>NatureOps</h2>  
-        </div>
-        <div className="sidebar">
-          <a href="https://www.google.es/">
-            <span> <ImNewspaper /></span>
-            <h3>News</h3>
-          </a>
-          <a href="https://www.google.es/">
-            <span> <MdEvent /></span>
-            <h3>Events</h3>
-          </a>
-          <a href="/sitios">
-            <span> <MdPlace /></span>
-            <h3>Places</h3>
-          </a>
-          <a href="https://www.google.es/">
-            <span> <CgProfile /></span>
-            <h3>Profile</h3>
-          </a>
-          <a className="salir" href="https://www.google.es/">
-            <span> <FiLogOut /></span>
-            <h3>Logout</h3>
-          </a>
-        </div>
-      </aside>
+      <Aside/>
       <main>
         <h1>Dashboard</h1>
         <div className="noticias">
--- src/components/Sitios.jsx
+++ src/components/Sitios.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 /*, { useState }*/
-/*import Aside from "./componentsDashBoard/Aside.jsx";*/
+import Aside from "./componentsDashBoard/Aside.jsx";
 /*ICONOS*/
-import { ImNewspaper } from "react-icons/im";
-import { MdEvent } from "react-icons/md";
-import { MdPlace } from "react-icons/md";
-import { CgProfile } from "react-icons/cg";
-import { ImCross} from "react-icons/im";
-import { FiLogOut } from "react-icons/fi";
+
 import { FiMenu } from "react-icons/fi";
 import {FaPlus} from "react-icons/fa";
 
@@ -28,45 +23,9 @@ function Sitios() {
     document.getElementById("asidee").classList.add("abrir");
   }
 
-  function cerrarNavbar(){
-
-    document.getElementById("asidee").classList.remove("abrir");
-    document.getElementById("asidee").classList.add("cerrar");
-  }
-
   return(
     <div className="container">
-      <aside id="asidee" className="cerrar">
-        <div className = "top">
-          <div className="close" id="close-btn">
-            <button onClick={cerrarNavbar}><ImCross /></button>
-          </div>
-          <img className="imagenLogo" src={logo} alt="Logo"/>
-          <h2>NatureOps</h2>  
-        </div>
-        <div className="sidebar">
-          <a href="https://www.google.es/">
-            <span> <ImNewspaper /></span>
-            <h3>News</h3>
-          </a>
-          <a href="https://www.google.es/">
-            <span> <MdEvent /></span>
-            <h3>Events</h3>
-          </a>
-          <a href="/sitios">
-            <span> <MdPlace /></span>
-            <h3>Places</h3>
-          </a>
-          <a href="https://www.google.es/">
-            <span> <CgProfile /></span>
-            <h3>Profile</h3>
-          </a>
-          <a className="salir" href="https://www.google.es/">
-            <span> <FiLogOut /></span>
-            <h3>Logout</h3>
-          </a>
-        </div>
-      </aside>
+      <Aside/>
       <main>
         <h1>Sitios</h1>
         <div className="listSitios">
@@ -74,7 +33,7 @@ function Sitios() {
             <img src={playa} alt="noticia1" className="imgSitios"/>
             <h4>Playa de la Concha</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure sunt numquam quidem, 
-            impedit quis ipsa fugit excepturi alias voluptatibus ratione.
+            impedit quis ipsa fugit excepturi alias voluptatibus rationePDLC.
             </p>
             <p>9.8</p>
           </div>
@@ -82,7 +41,7 @@ function Sitios() {
             <img src={playa} alt="noticia2" className="imgSitios"/>
             <h4>Playa Zurriola</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure sunt numquam quidem, 
-            impedit quis ipsa fugit excepturi alias voluptatibus ratione.
+            impedit quis ipsa fugit excepturi alias voluptatibus ratione.PZ
             </p>
             <p>8.4</p>
           </div>
@@ -90,17 +49,17 @@ function Sitios() {
             <img src={playa} alt="noticia2" className="imgSitios"/>
             <h4>Playa Santiago</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure sunt numquam quidem, 
-            impedit quis ipsa fugit excepturi alias voluptatibus ratione.
+            impedit quis ipsa fugit excepturi alias voluptatibus ratione.PS
             </p>
             <p>5.5</p>
           </div>
           <div className="sitioCard">
             <img src={playa} alt="noticia2" className="imgSitios"/>
-            <h4>Playa Santiago</h4>
+            <h4>Playa de Antilla</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure sunt numquam quidem, 
-            impedit quis ipsa fugit excepturi alias voluptatibus ratione.
+            impedit quis ipsa fugit excepturi alias voluptatibus ratione.PDA
             </p>
-            <p>5.5</p>
+            <p>5.7</p>
           </div>
         </div>
         <button className="buttonAdd"><FaPlus size="18px" color="white"/></button>
