b6ee574498010034a18ee136eb6ad48a157d4a65
oihane.epelde <oihane.epelde@alumni.mondragon.edu>
2022-05-26 22:48:18 +0200

    Json+minor fixes

--- src/components/Logros.jsx
+++ src/components/Logros.jsx
@@ -8,9 +8,9 @@ import { FiMenu } from "react-icons/fi";
 
 /*IMAGENES*/
 import logo from "../images/logo.png";
-import logro1 from "../images/logro_img.png";
+/*import logro1 from "../images/logro_img.png";
 import logro2 from "../images/logro_img2.png";
-import logro3 from "../images/logro_img3.png";
+import logro3 from "../images/logro_img3.png";*/
 
 
 
@@ -26,18 +26,17 @@ function Logros() {
     document.getElementById("asidee").classList.remove("cerrar");
     document.getElementById("asidee").classList.add("abrir");
   }
-
+  console.log(data);
   const DisplayData=data.map(
     (logro)=>{
       return(
         <div className="logroCard" key={logro.id}>
           <div className="logroTop">
-            <img src={logro1} alt="logro1" className="logroImg"/>
+            <img src={logro.imagen} alt="logro1" className="logroImg"/>
             <h4>{logro.nombre}</h4>
           </div>
           <p>{logro.descripcion}</p>
           <div className="progress-element">
-            <span>Progreso: </span>
             <div className="bar">
               <div style={{"width": logro.progresoUsuario + "%", "backgroundColor":"#48BFE3", "height":"20px"}}>
               </div>
@@ -58,96 +57,6 @@ function Logros() {
         <h1>Logros</h1>
         <div className="listLogros">
           {DisplayData}
-          <div className="logroCard">
-            <div className="logroTop">
-              <img src={logro1} alt="logro1" className="logroImg"/>
-              <h4>Nombre logro</h4>
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure sunt numquam quidem, 
-            impedit quis ipsa fugit excepturi alias voluptatibus ratione.
-            </p>
-            <div className="progress-element">
-              <span>Progreso: </span>
-              <div className="progress-container" id="p1">
-                <progress value="5" max="10">50%</progress>
-              </div>
-            </div>
-          </div>
-          <div className="logroCard">
-            <div className="logroTop">
-              <img src={logro2} alt="logro2" className="logroImg"/>
-              <h4>Nombre logro</h4>
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure sunt numquam quidem, 
-            impedit quis ipsa fugit excepturi alias voluptatibus ratione.
-            </p>
-            <div className="progress-element">
-              <span>Progreso: </span>
-              <div className="progress-container" id="p1">
-                <progress value="5" max="10">50%</progress>
-              </div>
-            </div>
-          </div>
-          <div className="logroCard">
-            <div className="logroTop">
-              <img src={logro3} alt="logro3" className="logroImg"/>
-              <h4>Nombre logro</h4>
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure sunt numquam quidem, 
-            impedit quis ipsa fugit excepturi alias voluptatibus ratione.
-            </p>
-            <div className="progress-element">
-              <span>Progreso: </span>
-              <div className="progress-container" id="p1">
-                <progress value="5" max="10">50%</progress>
-              </div>
-            </div>
-          </div>
-          <div className="logroCard">
-            <div className="logroTop">
-              <img src={logro2} alt="logro2" className="logroImg"/>
-              <h4>Nombre logro</h4>
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure sunt numquam quidem, 
-            impedit quis ipsa fugit excepturi alias voluptatibus ratione.
-            </p>
-            <div className="progress-element">
-              <span>Progreso: </span>
-              <div className="progress-container" id="p1">
-                <progress value="5" max="10">50%</progress>
-              </div>
-            </div>
-          </div>
-          <div className="logroCard">
-            <div className="logroTop">
-              <img src={logro3} alt="logro3" className="logroImg"/>
-              <h4>Nombre logro</h4>
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure sunt numquam quidem, 
-            impedit quis ipsa fugit excepturi alias voluptatibus ratione.
-            </p>
-            <div className="progress-element">
-              <span>Progreso: </span>
-              <div className="progress-container" id="p1">
-                <progress value="5" max="10">50%</progress>
-              </div>
-            </div>
-          </div>
-          <div className="logroCard">
-            <div className="logroTop">
-              <img src={logro1} alt="logro1" className="logroImg"/>
-              <h4>Nombre logro</h4>
-            </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure sunt numquam quidem, 
-            impedit quis ipsa fugit excepturi alias voluptatibus ratione.
-            </p>
-            <div className="progress-element">
-              <span>Progreso: </span>
-              <div className="progress-container" id="p1">
-                <progress value="5" max="10">50%</progress>
-              </div>
-            </div>
-          </div>
         </div>
       </main>
       <div className="right">
