f3ec03f2b8fbfd8f6132e22c88089f9839099d08
Unai Rodriguez <unai.rodriguezi@alumni.mondragon.edu>
2022-06-03 08:16:30 +0200

    news backend finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./components/Login.jsx";
 import LandingPage from "./components/LandingPage.jsx";
 import Registro from "./components/Registro.jsx";
 import DashBoard from "./components/Dashboard.jsx";
+import Noticias from "./components/Noticias.jsx";
 
 export function App() {
   return (
@@ -13,6 +14,7 @@ export function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
         <Route path="/dashboard" element={<DashBoard />}/>
+        <Route path="/noticias" element={<Noticias />}/>
       </Routes>
     </BrowserRouter>
   );
--- src/components/Noticias.jsx
+++ src/components/Noticias.jsx
@@ -1,24 +1,61 @@
-import React from "react";
-import NoticiasJson from "../jsons/noticias.json"
-
-
-
+import React , {useState, useEffect}from "react";
+import axios from "../api/axios";
+import ReactPaginate from "react-paginate";
 
 /*IMAGENES*/
 
 import "../styles/styleSidebar.css"
 import Aside from "./componentsDashBoard/Aside.jsx" 
-import PaginationComponent from "./componentsDashBoard/PaginationComponent.jsx";
 
 function Noticias() {
 
+  const [items, setItems] = useState([]);
+  const [pageCount, setpageCount] = useState(0);
+
+
+
+ 
+
+  /*const fetchSize = async () =>{
+    const response = await axios.get("api/news/size");
+    setSize(response?.data); 
+    return response;
+  }*/
+  useEffect(() =>{
+
+    const fetchData = async () =>{
+      const response = await axios.get("api/news/0/8");
+      setItems(response.data);
+    }
+
+    const fetchSize = async () =>{
+      const responseSize = await axios.get("api/news/size");
+      setpageCount(responseSize.data/8);
+    }
+   
+    fetchData();
+    fetchSize();
+   
+  },[])
+
+  const handlePageClick = async (data) => {
+    const fetchData = async () =>{
+     
+      console.log(data);
+      let url = "api/news/" + data.selected + "/8";
+      const response = await axios.get(url);
+      setItems(response.data);
+    }
+    fetchData();
+  }
+ 
   const cargarImagenNoti = require.context("../images", true);
-  const DisplayNoticias = NoticiasJson.map(record=>{
+  const DisplayNoticias = items.map(record=>{
     return(
       <div className="card" key={record.id} itemsPerPage="8">
-        <img src={cargarImagenNoti(`./${record.imagen}`)} alt={record.id}/>
-        <h4>{record.titulo}</h4>
-        <p>{record.descripcion}</p>
+        <img src={cargarImagenNoti(`./${record.image}`)} alt={record.id}/>
+        <h4>{record.title}</h4>
+        <p>{record.subtitle}</p>
         <a href={record.URL}>Leer más</a> 
       </div>
     )
@@ -29,8 +66,23 @@ function Noticias() {
       <main>
         <h1>Noticias</h1>
         <div className="noticias">
-          <PaginationComponent DisplayData={DisplayNoticias} itemsPerPage="8"/>
+          {DisplayNoticias}
+
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel="SIGUIENTE >"
+            onPageChange={handlePageClick}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={3}
+            pageCount={pageCount}
+            previousLabel="< ANTERIOR"
+            renderOnZeroPageCount={null}
+            containerClassName="pagination"
+            nextLinkClassName="next"
+            previousLinkClassName="previous"
+          />
         </div>
+
       </main>
     </div>
   );
