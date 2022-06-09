94f6420204b7e9b7f06fc4d77e8d4834a342e495
oihane.epelde <oihane.epelde@alumni.mondragon.edu>
2022-06-09 10:26:41 +0200

    crearEvento terminado

--- src/pages/CrearEvento.jsx
+++ src/pages/CrearEvento.jsx
@@ -3,48 +3,51 @@ import FormLugarComponente from "./componentsDashBoard/FormLugarComponent.jsx";
 import FormNuevoLugarComponente from "./componentsDashBoard/FormNuevoLugarComponent.jsx";
 import { FiCheck } from "react-icons/fi"
 import { VscChromeClose } from "react-icons/vsc"
+import { useTranslation } from "react-i18next";
+
 
 import "../styles/styleGeneralForms.css"
-import "../styles/crearEvento.css"
+import styleCrearEvento from "../styles/crearEvento.module.css"
 
 import lugares from "../jsons/nombresSitios.json"
 
 function CrearEvento() {
 
-  return(
-    <div className="claseSubirImagen" data-testid="eventoaSortu">  
-     
-      <div className="forma evento">
-        <h1>Crear evento</h1>
+  const { t } = useTranslation();
+
+  return( 
+    <main className={styleCrearEvento.eventoContainer}>
+      <div className={"forma".concat(" "+styleCrearEvento.evento)} data-testid="eventoaSortu">
+        <h1>{t("CreateEvent.CreateEvent")}</h1>
         <div className="formImagen">
-          <label htmlFor="fname">Nombre: </label>
-          <input type="text" id="fname" name="fname" className="inputClass" required/>
+          <label htmlFor="fname">{t("CreateEvent.NameNewEvent")}: </label>
+          <input type="text" id="fname" className="inputClass" required/>
         </div>
 
-        <FormLugarComponente lugares={lugares} labelName="Lugar: "/>
-        <FormNuevoLugarComponente/>
+        <FormLugarComponente lugares={lugares} labelName={t("CreateEvent.PlaceNewEvent").concat(":")}/>
+        <FormNuevoLugarComponente labelName={t("CreateEvent.Question")} inputPlaceholder={t("CreateEvent.NameNewEvent")}/>
           
-        <div className="fechaHora">
+        <div className={styleCrearEvento.fechaHora}>
           <div className="formImagen">
-            <label htmlFor="fname">Fecha: </label>
-            <input type="date" id="fname" name="fname" className="inputClass"/>
+            <label htmlFor="fFecha">{t("CreateEvent.DateNewEvent")}: </label>
+            <input type="date" id="fFecha" className="inputClass"/>
           </div>
           <div className="formImagen">
-            <label htmlFor="fname">Hora: </label>
-            <input type="time" id="fname" name="fname" className="inputClass"/>
+            <label htmlFor="fHora">{t("CreateEvent.TimeNewEvent")}: </label>
+            <input type="time" id="fHora" className="inputClass"/>
           </div>
         </div>
         <div className="formImagen">
-          <label htmlFor="fname">Descripcion: </label>
-          <textarea id="fname" name="fname" className="inputClass"/>
+          <label htmlFor="fDescripcion">{t("CreateEvent.DescriptionNewEvent")}: </label>
+          <textarea id="fDescripcion" className="inputClass"/>
         </div>
         <div className="panelBotones">
-          <button className="accept">ACEPTAR<FiCheck/></button>
-          <button className="cancel">CANCELAR<VscChromeClose/></button>
+          <button className="accept">{t("CreateEvent.Accept")}<FiCheck/></button>
+          <button className="cancel">{t("CreateEvent.Cancel")}<VscChromeClose/></button>
         </div>
-      </div>
-                
-    </div>
+      </div>  
+    </main>    
+     
   );
 }
 
--- src/pages/componentsDashBoard/FormNuevoLugarComponent.jsx
+++ src/pages/componentsDashBoard/FormNuevoLugarComponent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function FormNuevoLugarComponente(){
+function FormNuevoLugarComponente({labelName, inputPlaceholder}){
   function mostrarTexto() {
     var checkBox = document.getElementById("checkSitio");
     var text = document.getElementById("inputOculto");
@@ -16,9 +16,9 @@ function FormNuevoLugarComponente(){
     <div className="formNuevoLugar">
       <div className="formCheck">
         <input type="checkbox" id="checkSitio" name="vehicle1" value="Quiero registrar un nuevo sitio" className="inputClass" onClick={mostrarTexto}></input>
-        <label htmlFor="lname"> Quiero registrar un nuevo lugar </label>
+        <label htmlFor="lname">{labelName}</label>
       </div>
-      <input type="text" id="inputOculto" name="lname" className="inputClass oculto" placeholder="Nombre del sitio"/>  
+      <input type="text" id="inputOculto" name="lname" className="inputClass oculto" placeholder={inputPlaceholder}/>  
     </div>
   );
 }
