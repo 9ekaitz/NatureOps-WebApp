1957cd671bd578fc1309043e5acf924ff1cc44e5
oihane.epelde <oihane.epelde@alumni.mondragon.edu>
2022-06-11 21:12:03 +0200

    pagination cambiado

--- src/pages/Sitios.jsx
+++ src/pages/Sitios.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 import { useTranslation } from "react-i18next";
 
 import styleSitio from "../styles/styleSitios.module.css"
 import data from "../data/sitios.json";
-import PaginationComponent from "../components/componentsDashBoard/PaginationComponent.jsx";
+import style from "../styles/pagination.module.css"
 
 function Sitios() {
 
   const { t } = useTranslation();
+
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const itemsPerPage="6";
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % DisplayData.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    setCurrentItems(DisplayData.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(DisplayData.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage]);
 
   const DisplayData=data.map(
     (sitio)=>{
@@ -26,7 +48,20 @@ function Sitios() {
     <main className={styleSitio.containerSitios} data-testid="container">
       <h1>{t("Places.Places")}</h1>
       <div className={styleSitio.listSitios}>
-        <PaginationComponent DisplayData={DisplayData} itemsPerPage="6" stylePantalla={styleSitio}/>
+        {currentItems}
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel=">"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="<"
+          renderOnZeroPageCount={null}
+          containerClassName={style.pagination.concat(" "+styleSitio.pagination)}
+          nextLinkClassName={style.next}
+          previousLinkClassName={style.previous}
+          activeClassName={style.active}
+        />
       </div>
     </main>
   );
