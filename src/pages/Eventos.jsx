ca1a80b3448a819cbb87967f46db3d2c6ce01990
oihane.epelde <oihane.epelde@alumni.mondragon.edu>
2022-06-11 20:35:13 +0200

    pagination cambiado

--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaPlus } from "react-icons/fa"
 import { useTranslation } from "react-i18next";
-
-import PaginationComponent from "../components/componentsDashBoard/PaginationComponent.jsx";
+import ReactPaginate from "react-paginate";
 
 import styleEventos from "../styles/styleEventos.module.css"
+import style from"../styles/pagination.module.css"
+
 
 import data from "../data/eventos.json";
 
 
 function Eventos() {
   const { t } = useTranslation();
+
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const itemsPerPage="8";
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % DisplayData.length;
+    setItemOffset(newOffset);
+  };
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(DisplayData.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(DisplayData.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage]);
 
   const DisplayData=[];
   let DisplayDataItem={};
@@ -38,7 +56,20 @@ function Eventos() {
       <div className={styleEventos.eventos}>
         <h2>{t("Events.List")}</h2>
         <div className={styleEventos.listaEventos}>
-          <PaginationComponent DisplayData={DisplayData} itemsPerPage="8" stylePantalla={styleEventos}/>         
+          {currentItems}
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel=">"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={5}
+            pageCount={pageCount}
+            previousLabel="<"
+            renderOnZeroPageCount={null}
+            containerClassName={style.pagination.concat(" "+styleEventos.pagination)}
+            nextLinkClassName={style.next}
+            previousLinkClassName={style.previous}
+            activeClassName={style.active}
+          />        
         </div>
         <button className={styleEventos.addButton}><FaPlus className={styleEventos.icon} size="17px"/></button>
       </div>
